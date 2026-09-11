b64fd2037999151bb798f008fd789fb0c23e4996
alexnevess <alexsndsf@gmail.com>
2026-09-10 23:05:31 -0400

    runs the database

--- database/database.py
+++ database/database.py
@@ -3,10 +3,13 @@ import sqlite3
 con = sqlite3.connect("database.db")
 
 cursor = con.cursor()
-cursor.execute("""CREATE TABLE customer(
+cursor.execute("""CREATE TABLE IF NOT EXISTS customer(
                     id integer PRIMARY KEY,
                     name TEXT NOT NULL,
                     CPF TEXT UNIQUE NOT NULL,
                     age integer NOT NULL,
                     email TEXT NOT NULL,
                     date TEXT NOT NULL)""")
+
+con.commit()
+con.close()
