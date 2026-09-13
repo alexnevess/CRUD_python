e0fd4a521c3b8eafd2bdbe655a330aa42d44a49d
Alexsandro Filho <alexsndsf@gmail.com>
2026-09-13 13:01:33 -0300

    Add name validation

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from database import database
 
-Name = input("Nome:")
-cpf = input("CPF:")
-age = int(input("Idade:"))
-email = input("Email:")
-date = input("Data")
+while True: # Utilizalção de while true break para validação de name
+    len_name = 0
+    name = input("Nome:")
+    len_name = len(name)
+    if len_name < 20 and len_name > 0 and name.replace(" ","").isalpha(): # Utiliza replace (retira os espaços) e isalpha para validar se os caracteres são válidos (apenas letras)
+        break
+    else:
+        print("Digite um nome válido!!!")
+        
+# cpf = input("CPF:")
+# age = int(input("Idade:"))
+# email = input("Email:")
+# date = input("Data")
 
